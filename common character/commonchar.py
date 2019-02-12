3b7e45800b9736121ad98e835d9031efc83583e2
samyak360 <samyak360@gmail.com>
2019-02-12 12:50:22 +0530

    added strings in print in common char

--- common character/commonchar.py
+++ common character/commonchar.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-testcase=int(input())
+testcase=int(input("enter no of test case:"))
 for z in range(testcase):
-	n=int(input())
+	n=int(input("enter no of strings you will enter to count common characters in them:"))
 
 	d={}
 	count=0
 
 	for i in range(n):
 		d["a"+str(i)]=[0]*26
-		string=input()
+		string=input("enter string {} : ".format(i+1))
 		length=len(string)
 		for j in range(length):
 			d["a"+str(i)][ord(string[j])-97]+=1
@@ -23,4 +23,4 @@ for z in range(testcase):
 		if(flag==0):
 			count+=1
 
-	print(count)
+	print("no of common char in above all strings are :",count)
